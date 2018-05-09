eadb3a9e9d09145ce3fedca2b5ab940f0d42c2bf
suolong520 <1172468462@qq.com>
2018-05-09 15:27:16 +0800

    0

--- public/Wechat/js/goodslist.js
+++ public/Wechat/js/goodslist.js
@@ -49,7 +49,7 @@ $(function(){
                   // 删除加载提示符
                   $('.infinite-scroll-preloader').remove();
                   return;
-
+                  console.log("0");
               // 添加新条目
               // 更新最后加载的序号
               //容器发生改变,如果是js滚动，需要刷新滚动
