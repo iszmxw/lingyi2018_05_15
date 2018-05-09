7566751dd360d6d5011d2fb77405e8be22e0a0d6
kbzz <463661663@qq.com>
2018-05-09 17:29:08 +0800

    1

--- app/Http/Controllers/Api/WechatApiController.php
+++ app/Http/Controllers/Api/WechatApiController.php
@@ -18,6 +18,7 @@ use App\Models\SimpleCategory;
 use App\Models\SimpleConfig;
 use App\Models\SimpleGoods;
 use App\Models\SimpleGoodsThumb;
+use App\Models\SimpleSelftakeOrder;
 use App\Models\SimpleStock;
 use App\Models\SimpleStockLog;
 use App\Services\ZeroneRedis\ZeroneRedis;
