6856ab59b6c2cf77d6e5203bc87171439b32acfb
xho <781966658.com>
2018-05-09 16:06:05 +0800

    x

--- routes/web.php
+++ routes/web.php
@@ -988,8 +988,6 @@ Route::group(['prefix' => 'pay'], function () {
         Route::any('test11', 'Pay\SftController@test11')->middleware("UserCheck");//测试函数
         Route::any('test12', 'Pay\SftController@test12');//测试函数
 
-
-
         Route::any('test13', 'Pay\WxController@test13');//测试函数
         Route::any('test14', 'Pay\WxController@test14');//测试函数
         Route::any('test15', 'Pay\WxController@test15');//测试函数
