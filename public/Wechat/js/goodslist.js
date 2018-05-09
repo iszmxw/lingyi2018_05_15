b5f73f5c3768f017320fd656b9ad10e3bf12de11
suolong520 <1172468462@qq.com>
2018-05-09 11:17:12 +0800

    0

--- public/Wechat/js/goodslist.js
+++ public/Wechat/js/goodslist.js
@@ -76,19 +76,19 @@ $(function(){
         //     //mescroll.endByPage(curPageData.length, totalPage); //必传参数(当前页的数据个数, 总页数)
         //
         //     //方法二(推荐): 后台接口有返回列表的总数据量 totalSize
-        //     //mescroll.endBySize(curPageData.length, totalSize); //必传参数(当前页的数据个数, 总数据量)
+            mescroll.endBySize(20, 5); //必传参数(当前页的数据个数, 总数据量)
         //
         //     //方法三(推荐): 您有其他方式知道是否有下一页 hasNext
         //     //mescroll.endSuccess(curPageData.length, hasNext); //必传参数(当前页的数据个数, 是否有下一页true/false)
         //
         //     //方法四 (不推荐),会存在一个小问题:比如列表共有20条数据,每页加载10条,共2页.如果只根据当前页的数据个数判断,则需翻到第三页才会知道无更多数据,如果传了hasNext,则翻到第二页即可显示无更多数据.
-            mescroll.endSuccess(20);
+        //     mescroll.endSuccess(curPageData.length);
         //
         //     //设置列表数据
         //     setListData(curPageData, true);
         // }, function(){
         //     //联网失败的回调,隐藏下拉刷新和上拉加载的状态;
-        //     mescroll.endErr();
+            mescroll.endErr();
         // });
     }
 });
