789193fb399c65a74b917f0fa166e09fa0eb4f44
xho <781966658.com>
2018-05-09 14:12:38 +0800

    x

--- resources/views/Fansmanage/Test/test.blade.php
+++ resources/views/Fansmanage/Test/test.blade.php
@@ -13,7 +13,7 @@
 
 <h1>订单支付</h1>
 
-<button onclick="jsApiCall()">
+<button onclick="callpay()">
     <h4>
         确定支付
     </h4>
@@ -58,23 +58,23 @@
     // });
     // })
 
-    function jsApiCall() {
-        WeixinJSBridge.invoke(
-            'getBrandWCPayRequest',
-            {
-                appId: '{{$wxpay["appid"]}}',
-                timestamp: '{{$wxpay["appid"]}}', // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
-                nonceStr: '{{$wxpay["nonce_str"]}}', // 支付签名随机串，不长于 32 位
-                package: 'prepay_id={{$wxpay["prepay_id"]}}', // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=***）
-                signType: 'MD5', // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'
-                paySign: '{{$wxpay["sign"]}}', // 支付签名
-            },
-            function (res) {
-                WeixinJSBridge.log(res.err_msg);
-                // alert(res.err_code+res.err_desc+res.err_msg);
-            }
-        );
-    }
+    {{--function jsApiCall() {--}}
+        {{--WeixinJSBridge.invoke(--}}
+            {{--'getBrandWCPayRequest',--}}
+            {{--{--}}
+                {{--appId: '{{$wxpay["appid"]}}',--}}
+                {{--timestamp: '{{$wxpay["appid"]}}', // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符--}}
+                {{--nonceStr: '{{$wxpay["nonce_str"]}}', // 支付签名随机串，不长于 32 位--}}
+                {{--package: 'prepay_id={{$wxpay["prepay_id"]}}', // 统一支付接口返回的prepay_id参数值，提交格式如：prepay_id=***）--}}
+                {{--signType: 'MD5', // 签名方式，默认为'SHA1'，使用新版支付需传入'MD5'--}}
+                {{--paySign: '{{$wxpay["sign"]}}', // 支付签名--}}
+            {{--},--}}
+            {{--function (res) {--}}
+                {{--WeixinJSBridge.log(res.err_msg);--}}
+                {{--// alert(res.err_code+res.err_desc+res.err_msg);--}}
+            {{--}--}}
+        {{--);--}}
+    {{--}--}}
 
     function callpay() {
         console.log(123);
