29fd70a662989a27a99bbcaf00b6771e250bef01
xho <781966658.com>
2018-05-09 15:11:48 +0800

    x

--- resources/views/Fansmanage/Test/test.blade.php
+++ resources/views/Fansmanage/Test/test.blade.php
@@ -31,9 +31,6 @@
         signature: '{{$signPackage["signature"]}}',// 必填，签名
         jsApiList: [
             'checkJsApi',
-            'onMenuShareTimeline',
-            'onMenuShareAppMessage',
-            'openAddress',
             'chooseWXPay',
         ] // 必填，需要使用的JS接口列表
     });
@@ -77,25 +74,7 @@
     {{--}--}}
 
     function callpay() {
-        console.log(123);
         wx.ready(function () {
-
-            wx.checkJsApi({
-                jsApiList: [
-                    'checkJsApi',
-                    'onMenuShareTimeline',
-                    'onMenuShareAppMessage',
-                    'openAddress',
-                    'chooseWXPay'
-                ], // 需要检测的JS接口列表，所有JS接口列表见附录2,
-                success: function (res) {
-                    console.log(res);
-                    // 以键值对的形式返回，可用的api值true，不可用为false
-                    // 如：{"checkResult":{"chooseImage":true},"errMsg":"checkJsApi:ok"}
-                }
-            });
-
-
             wx.chooseWXPay({
                 appId: '{{$wxpay["appid"]}}',
                 timestamp: '{{$wxpay["timestamp"]}}', // 支付签名时间戳，注意微信jssdk中的所有使用timestamp字段均为小写。但最新版的支付后台生成签名使用的timeStamp字段名需大写其中的S字符
