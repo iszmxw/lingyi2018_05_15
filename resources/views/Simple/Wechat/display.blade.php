ff032e47f858c278514eef9010888ac0e8e13a4e
suolong520 <1172468462@qq.com>
2018-05-09 16:26:45 +0800

    0

--- resources/views/Simple/Wechat/display.blade.php
+++ resources/views/Simple/Wechat/display.blade.php
@@ -66,13 +66,12 @@
     </div>
 
 	<script type='text/javascript' src="http://res.wx.qq.com/open/js/jweixin-1.0.0.js" charset='utf-8'></script>
-    <script src="https://www.w3cways.com/demo/vconsole/vconsole.min.js?v=2.2.0"></script>
     <script type='text/javascript' src="{{asset('public/Wechat')}}/js/public/jquery.min.js" charset='utf-8'></script>
     <script type='text/javascript' src="{{asset('public/Wechat')}}/js/public/light7.min.js" charset='utf-8'></script>
     <script type='text/javascript' src="{{asset('public/Wechat')}}/js/selectshop.js" charset='utf-8'></script>
     <script type="text/javascript">
      wx.config({
-	      debug: true,
+	      debug: false,
           appId:'{{$appId}}',
           timestamp: '{{$timestamp}}',
           nonceStr: '{{$nonceStr}}',
