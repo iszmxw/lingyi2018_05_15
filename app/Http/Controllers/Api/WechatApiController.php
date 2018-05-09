9fefa925f520489ebf814e39c94ebda1de8ef78b
kbzz <463661663@qq.com>
2018-05-09 14:52:31 +0800

    1

--- app/Http/Controllers/Api/WechatApiController.php
+++ app/Http/Controllers/Api/WechatApiController.php
@@ -145,7 +145,7 @@ class WechatApiController extends Controller
             return response()->json(['status' => '0', 'msg' => '没有商品', 'data' => '']);
         }
         foreach ($goodslist as $key => $value) {
-            $goodslist[$key]['price'] = round($value['price'],2);
+            $goodslist[$key]['price'] = round($value['price'], 2);
             $goodslist[$key]['category_name'] = SimpleCategory::getPluck([['id', $value['category_id']]], 'name');
             $goodslist[$key]['thumb'] = SimpleGoodsThumb::where([['goods_id', $value['id']]])->select('thumb')->get();
             if (count($goodslist[$key]['thumb']) == 0) {
@@ -888,6 +888,19 @@ class WechatApiController extends Controller
      */
     public function order_submit(Request $request)
     {
+
+
+        $address_info = [
+            "province_name" => "广东省",
+            "city_name" => "深圳市",
+            "district_name" => "龙岗区",
+            "address" => "万汇大厦1606",
+            "realname" => "kbzz",
+            "mobile" => "15277049514",
+            "status" => "1",
+        ];
+
+
         // 用户id
         $user_id = $request->user_id;
         // 零壹id
@@ -923,7 +936,7 @@ class WechatApiController extends Controller
         }
 
         // 查询订单今天的数量
-        $num = SimpleOnlineOrder::where([['fansmanage_id',$fansmanage_id],['simple_id', $store_id], ['ordersn', 'LIKE', '%' . date("Ymd", time()) . '%']])->count();
+        $num = SimpleOnlineOrder::where([['fansmanage_id', $fansmanage_id], ['simple_id', $store_id], ['ordersn', 'LIKE', '%' . date("Ymd", time()) . '%']])->count();
         $num += 1;
         $sort = 100000 + $num;
         // 订单号
@@ -964,13 +977,13 @@ class WechatApiController extends Controller
             }
 
             $address_data = [
-                'order_id' =>$order_id,
-                'province_name' =>$address_info['province_name'],
-                'city_name' =>$address_info['city_name'],
-                'district_name' =>$address_info['district_name'],
-                'address' =>$address_info['address'],
-                'relaname' =>$address_info['relaname'],
-                'mobile' =>$address_info['relaname'],
+                'order_id' => $order_id,
+                'province_name' => $address_info['province_name'],
+                'city_name' => $address_info['city_name'],
+                'district_name' => $address_info['district_name'],
+                'address' => $address_info['address'],
+                'relaname' => $address_info['relaname'],
+                'mobile' => $address_info['relaname'],
             ];
             SimpleOnlineAddress::addSimpleOnlineAddress($address_data);//添加商品快照
             // 提交事务
@@ -1084,7 +1097,6 @@ class WechatApiController extends Controller
     }
 
 
-
     /**
      * 减库存
      * @order_id 订单id
@@ -1172,16 +1184,6 @@ class WechatApiController extends Controller
         }
         return 'ok';
     }
-
-
-
-
-
-
-
-
-
-
 
 
 }
--- app/Http/Middleware/Api/WechatApiCheck.php
+++ app/Http/Middleware/Api/WechatApiCheck.php
@@ -391,6 +391,27 @@ class WechatApiCheck
      */
     public function checkOrderSubmit($request)
     {
+
+        $goods_list = [
+            [
+                "goods_id" => "22",
+                "goods_name" => "优酸乳",
+                "goods_thumb" => "uploads/simple/20180504065214953.jpg",
+                "goods_price" => "11",
+                "num" => 29,
+                "stock" => 971
+            ],
+            [
+                "goods_id" => "20",
+                "goods_name" => "青菜肉丝面",
+                "goods_thumb" => "http://develop.01nnt.com/public/thumb.png",
+                "goods_price" => "11.00",
+                "num" => 62,
+                "stock" => 938
+
+            ]
+        ];
+        echo json_encode($goods_list);exit;
         if (empty($request->input('user_id'))) {
             return self::res(0, response()->json(['msg' => '用户ID不能为空', 'status' => '0', 'data' => '']));
         }
