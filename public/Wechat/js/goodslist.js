fa0eb5b31886d66f9288196fb97e0a97300a08f7
suolong520 <1172468462@qq.com>
2018-05-09 15:36:18 +0800

    0

--- public/Wechat/js/goodslist.js
+++ public/Wechat/js/goodslist.js
@@ -29,11 +29,11 @@ $(function(){
             }
 		}
 	);
-$.showPreloader();
+    $.showPreloader();
     //查询商品列表和购物车列表(category(默认为0 全部,keyword_val搜索值默认为空))
     selectgoods(0,"");
     $.hidePreloader();
-    //$.init();
+    $.init();
 });
 
 //查询商品列表和购物车列表
