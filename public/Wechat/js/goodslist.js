aa40208b844aded267b13a33739ac595cbc37bfc
suolong520 <1172468462@qq.com>
2018-05-09 14:20:35 +0800

    0

--- public/Wechat/js/goodslist.js
+++ public/Wechat/js/goodslist.js
@@ -92,9 +92,9 @@ $(function(){
             //setListData(curPageData, true);
         //}, function(){
             //联网失败的回调,隐藏下拉刷新和上拉加载的状态;
-            // setTimeout(function(){
-            //     mescroll.endErr();
-            // },1000);
+            setTimeout(function(){
+                mescroll.endErr();
+            },1000);
         //});
     }
 
