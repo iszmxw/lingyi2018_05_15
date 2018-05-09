5cb2508dbfdd1f12bee62236354b0b630bbffc1d
xho <781966658.com>
2018-05-09 13:55:14 +0800

    x

--- resources/views/Fansmanage/Test/test.blade.php
+++ resources/views/Fansmanage/Test/test.blade.php
@@ -1,11 +1,11 @@
 <html>
 <head>
     <meta name="viewport" id="viewport" content="width=device-width, initial-scale=1">
-    <meta name="apple-mobile-web-app-capable" content="yes" />
-    <meta name="apple-mobile-web-app-status-bar-style" content="black" />
-    <meta charset="utf-8">
-    <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport">
-    <meta content="telephone=no" name="format-detection">
+    {{--<meta name="apple-mobile-web-app-capable" content="yes" />--}}
+    {{--<meta name="apple-mobile-web-app-status-bar-style" content="black" />--}}
+    {{--<meta charset="utf-8">--}}
+    {{--<meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport">--}}
+    {{--<meta content="telephone=no" name="format-detection">--}}
 
 </head>
 
