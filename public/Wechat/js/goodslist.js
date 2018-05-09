626bfc589d5d66634c5759c06acae80047327c81
suolong520 <1172468462@qq.com>
2018-05-09 11:40:39 +0800

    0

--- public/Wechat/js/goodslist.js
+++ public/Wechat/js/goodslist.js
@@ -40,10 +40,6 @@ $(function(){
             auto: true, //是否在初始化时以上拉加载的方式自动加载第一页数据; 默认false
             isBounce: false, //此处禁止ios回弹,解析(务必认真阅读,特别是最后一点): http://www.mescroll.com/qa.html#q10
             callback: upCallback, //上拉回调,此处可简写; 相当于 callback: function (page) { upCallback(page); }
-            toTop:{ //配置回到顶部按钮
-                src : "../res/img/mescroll-totop.png", //默认滚动到1000px显示,可配置offset修改
-                //offset : 1000
-            }
         }
     });
     /*下拉刷新的回调 */
@@ -52,14 +48,17 @@ $(function(){
         //联网加载数据
         //getListDataFromNet(0, 1, function(data){
             //联网成功的回调,隐藏下拉刷新的状态
-            mescroll.endSuccess();
+            setTimeout(function(){
+                mescroll.endSuccess();
+            },1000);
+
             //设置列表数据
             //setListData(data, false);
         //}, function(){
             //联网失败的回调,隐藏下拉刷新的状态
-            setTimeout(function(){
-                mescroll.endErr();
-            },1000);
+            // setTimeout(function(){
+            //     mescroll.endErr();
+            // },1000);
         //});
     }
 
@@ -83,14 +82,17 @@ $(function(){
 
             //方法四 (不推荐),会存在一个小问题:比如列表共有20条数据,每页加载10条,共2页.如果只根据当前页的数据个数判断,则需翻到第三页才会知道无更多数据,如果传了hasNext,则翻到第二页即可显示无更多数据.
             //mescroll.endSuccess(curPageData.length);
+            setTimeout(function(){
+                        mescroll.endSuccess(20);
+            },1000);
 
             //设置列表数据
             //setListData(curPageData, true);
         //}, function(){
             //联网失败的回调,隐藏下拉刷新和上拉加载的状态;
-            setTimeout(function(){
-                mescroll.endErr();
-            },1000);
+            // setTimeout(function(){
+            //     mescroll.endErr();
+            // },1000);
         //});
     }
 
