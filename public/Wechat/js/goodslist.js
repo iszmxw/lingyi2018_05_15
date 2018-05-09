931989e031d4979d7f27ffe0c95af4c92544d2a8
suolong520 <1172468462@qq.com>
2018-05-09 11:27:09 +0800

    0

--- public/Wechat/js/goodslist.js
+++ public/Wechat/js/goodslist.js
@@ -57,7 +57,7 @@ $(function(){
             //setListData(data, false);
         //}, function(){
             //联网失败的回调,隐藏下拉刷新的状态
-            mescroll.endErr();
+            //mescroll.endErr();
         //});
     }
 
@@ -86,7 +86,7 @@ $(function(){
             //setListData(curPageData, true);
         //}, function(){
             //联网失败的回调,隐藏下拉刷新和上拉加载的状态;
-            mescroll.endErr();
+            //mescroll.endErr();
         //});
     }
 
