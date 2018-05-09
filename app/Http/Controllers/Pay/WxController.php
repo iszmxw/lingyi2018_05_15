f9209b391a4da97f6cd9ad07a19f51625ede7640
xho <781966658.com>
2018-05-09 17:26:25 +0800

    x

--- app/Http/Controllers/Pay/WxController.php
+++ app/Http/Controllers/Pay/WxController.php
@@ -148,7 +148,7 @@ class WxController extends Controller
 //        var_dump($res);
         echo $res["data"];
         file_put_contents("./uploads/1.csv",$res["data"]);
-        $fileName = ".uploads/1.csv"; //得到文件名
+        $fileName = "./uploads/1.csv"; //得到文件名
         header( "Content-Disposition:  attachment;  filename=".$fileName); //告诉浏览器通过附件形式来处理文件
         header('Content-Length: ' . filesize($fileName)); //下载文件大小
         readfile($fileName);  //读取文件内容
