56dc376e811e5b1209548d1856398f8bf2b19481
xho <781966658.com>
2018-05-10 13:54:55 +0800

    x

--- resources/views/Simple/Wechat/goodslist.blade.php
+++ resources/views/Simple/Wechat/goodslist.blade.php
@@ -107,6 +107,6 @@
     <script type='text/javascript' src="{{asset('public/Wechat')}}/js/public/jquery.min.js" charset='utf-8'></script>
     <script type='text/javascript' src="{{asset('public/Wechat')}}/js/public/light7.min.js" charset='utf-8'></script>
     <script type='text/javascript' src="{{asset('public/Wechat')}}/js/public/dropload.min.js" charset='utf-8'></script>
-    <script type='text/javascript' src="{{asset('public/Wechat')}}/js/goodslist.js?v222" charset='utf-8'></script>
+    <script type='text/javascript' src="{{asset('public/Wechat')}}/js/goodslist.js?v=<?php echo time(); ?>" charset='utf-8'></script>
   </body>
 </html>
