e9fcf0bd1f0d5df7ca6365980cbd521a2c0196e6
xho <781966658.com>
2018-05-10 14:23:34 +0800

    x

--- resources/views/Simple/Wechat/goodslist.blade.php
+++ resources/views/Simple/Wechat/goodslist.blade.php
@@ -10,6 +10,12 @@
     <link rel="stylesheet" href="{{asset('public/Wechat')}}/css/public/light7.min.css">
     <link rel="stylesheet" href="{{asset('public/Wechat')}}/css/public/dropload.css">
     <link rel="stylesheet" href="{{asset('public/Wechat')}}/css/goodslist.css">
+
+    <style>
+        .goodslist {
+            overflow-y:auto;
+        }
+    </style>
 </head>
 <body>
 <input type="hidden" name="_token" id="_token" value="{{csrf_token()}}">
@@ -47,20 +53,20 @@
             </div>
         </div>
     </div>
-    {{--<div class="cart_bottom">--}}
-        {{--<div class="cart_fl">--}}
-            {{--<div class="cart_left">--}}
-                {{--<div class="cart_kuang clear_after" id="cart_btn_b" onclick="showcart('cart',this)">--}}
-                    {{--<div class="cart_kuang_img">--}}
-                        {{--<span class="goods_number" id="goods_totalnum" data-totalnum="0">0</span>--}}
-                    {{--</div>--}}
-                {{--</div>--}}
-                {{--<h3 class="cart_price" id="cart_price" data-totalprice="0">您还未选购商品哦~</h3>--}}
-            {{--</div>--}}
-            {{--<div class="cart_right"><a href="javascript:;" onclick="show('alert')">去结算 <i></i></a></div>--}}
-        {{--</div>--}}
-    {{--</div>--}}
-    <!-- alert -->
+{{--<div class="cart_bottom">--}}
+{{--<div class="cart_fl">--}}
+{{--<div class="cart_left">--}}
+{{--<div class="cart_kuang clear_after" id="cart_btn_b" onclick="showcart('cart',this)">--}}
+{{--<div class="cart_kuang_img">--}}
+{{--<span class="goods_number" id="goods_totalnum" data-totalnum="0">0</span>--}}
+{{--</div>--}}
+{{--</div>--}}
+{{--<h3 class="cart_price" id="cart_price" data-totalprice="0">您还未选购商品哦~</h3>--}}
+{{--</div>--}}
+{{--<div class="cart_right"><a href="javascript:;" onclick="show('alert')">去结算 <i></i></a></div>--}}
+{{--</div>--}}
+{{--</div>--}}
+<!-- alert -->
     <div class="popup_alert" id="alert">
         <div class="quhuo alert_width popup_alert_hook">
             <p class="quhuoinfo">确认清空购物车吗?</p>
