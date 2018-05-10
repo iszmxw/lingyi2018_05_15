d4696a0c52a28c82c6779eebd742c11a3b7a38aa
xho <781966658.com>
2018-05-10 14:40:04 +0800

    x

--- resources/views/Simple/Wechat/goodslist.blade.php
+++ resources/views/Simple/Wechat/goodslist.blade.php
@@ -8,7 +8,7 @@
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-status-bar-style" content="black">
     <link rel="stylesheet" href="{{asset('public/Wechat')}}/css/public/dropload.css">
-    {{--<link rel="stylesheet" href="{{asset('public/Wechat')}}/css/public/light7.min.css">--}}
+    <link rel="stylesheet" href="{{asset('public/Wechat')}}/css/public/light7.min.css">
 </head>
 <body>
 <input type="hidden" name="_token" id="_token" value="{{csrf_token()}}">
