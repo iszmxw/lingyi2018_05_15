3479a01c8888bd3b351d2857e54bd46dc9893502
xho <781966658.com>
2018-05-11 17:15:17 +0800

    x

--- app/Http/Controllers/Pay/WxController.php
+++ app/Http/Controllers/Pay/WxController.php
@@ -47,22 +47,22 @@ class WxController extends Controller
 //        echo $this->microOrder($data);
 //exit;
 //
-//        // 商户订单号
-//        $data["order_num"] = md5(time());
-//        // 用户openid
-//
-//        $data["openid"] = "oK2HF1Sy1qdRQyqg69pPN5-rirrg";
-//        // 收款用户姓名
-//        $data["bank_code"] = "郑旭宏";
-//        // 金额
-//        $data["order_money"] = 1;
-//        // 企业付款描述信息
-//        $data["remark"] = "还钱";
-//        // ip 地址
-//        $data["ip_address"] = "120.78.140.10";
-        $data["order_num"] = "907dbfee0e326540949f31c54edcddd6";
+
+        // 商户订单号
+        $data["order_num"] = md5(time());
+        // 用户openid
+        $data["bank_card_num"] = "6214837873289338";
+        // 收款用户姓名
+        $data["bank_card_name"] = "郑旭宏";
+        $data["bank_code"] = "1001";
+        // 金额
+        $data["order_money"] = 1;
+        // 企业付款描述信息
+        $data["remark"] = "还钱";
+        // ip 地址
+        $data["ip_address"] = "120.78.140.10";
         $data = json_encode($data, JSON_UNESCAPED_UNICODE);
-        echo $this->gettransferinfo($data);
+        echo $this->pay_bank($data);
     }
 
 
