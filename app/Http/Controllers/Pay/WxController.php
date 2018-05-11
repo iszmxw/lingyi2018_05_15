efae343d75a040c7aa5b10bdb03ad6f9f3746426
xho <781966658.com>
2018-05-11 14:17:48 +0800

    x

--- app/Http/Controllers/Pay/WxController.php
+++ app/Http/Controllers/Pay/WxController.php
@@ -406,6 +406,8 @@ class WxController extends Controller
         $data = $this->array2xml($data);
         // 发送请求
         $resXml = $this->httpRequest($url, $method, $data, [], $is_ssh);
+        var_dump($resXml);
+        exit;
         // 将XML 转化为 数组
         $param = $this->xml2array($resXml);
 
