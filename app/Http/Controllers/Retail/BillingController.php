44b11cdea6c72bf2689412d85f9fb3d47ca71b0b
mail@54zm.com <mail@54zm.com>
2018-05-11 15:29:24 +0800

    'lingyi'

--- app/Http/Controllers/Retail/BillingController.php
+++ app/Http/Controllers/Retail/BillingController.php
@@ -114,8 +114,7 @@ class BillingController extends Controller
         } elseif ($type == 3) {
             $order = RetailLossOrder::getOne(['id' => $order_id])->first();       //获取订单信息
         } elseif ($type == 4) {
-            $order = RetailOrder::getOne(['id' => $order_id])->first();       //获取订单信息
-            dd($request);
+            $order = RetailCheckOrder::getOne(['id' => $order_id])->first();       //获取订单信息
         }
         return view('Retail/Billing/order_list_details', ['order' => $order]);
     }
