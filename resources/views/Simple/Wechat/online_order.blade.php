866973dee7999656e090baf3d48b32705162e068
suolong520 <1172468462@qq.com>
2018-05-11 14:28:42 +0800

    0

--- resources/views/Simple/Wechat/online_order.blade.php
+++ resources/views/Simple/Wechat/online_order.blade.php
@@ -11,6 +11,10 @@
     <link rel="stylesheet" href="{{asset('public/Wechat')}}/css/onlineOrder15-5.css">
   </head>
   <body>
+    <input type="hidden" name="_token" id="_token" value="{{csrf_token()}}">
+    <input type="hidden" name="fansmanage_id" id="fansmanage_id" value="{{$fansmanage_id}}">
+    <input type="hidden" name="store_id" id="store_id" value="{{$store_id}}">
+    <input type="hidden" name="zerone_user_id" id="zerone_user_id" value="{{session("zerone_auth_info.zerone_user_id")}}">
     <div class="page">
 	    <div class="g-flexview">
 	        <div class="top">
