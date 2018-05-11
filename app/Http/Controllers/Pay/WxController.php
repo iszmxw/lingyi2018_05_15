823ee3f9b98685eac4af3fd8214f9cc9cb7acb44
xho <781966658.com>
2018-05-11 16:08:35 +0800

    x

--- app/Http/Controllers/Pay/WxController.php
+++ app/Http/Controllers/Pay/WxController.php
@@ -758,9 +758,7 @@ class WxController extends Controller
                 if (count($v) == 0) {
                     $result[$k] = '';
                 } else if (count($v) == 1) {
-                    var_dump(current($v));
-                    exit;
-                    $result[$k] = $v[0];
+                    $result[$k] = current($v);
                 } else {
                     throw new \Exception('Duplicate elements in XML. ' . $str);
                 }
