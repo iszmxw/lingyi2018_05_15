24b3bb6278510e3d626ff2f730cf576e3a6978de
suolong520 <1172468462@qq.com>
2018-05-11 17:36:58 +0800

    0

--- resources/views/Simple/Wechat/online_order.blade.php
+++ resources/views/Simple/Wechat/online_order.blade.php
@@ -26,7 +26,7 @@
 	        <div class="order order_m">
 				<div class="order_box">
 					<h3 class="order_state15-10">待支付</h3>
-			    	<div class="item" onclick="ress_list()">
+			    	<div class="item address_info_box" id="address_info_box" onclick="ress_list()">
 				    	<div class="row">
 					      <div class="col-20 item_info"><span>收货信息</span></div>
 					      <div class="col-80 item_info1 item_icon">
