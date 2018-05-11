a84f79834b7178555eaa70729d8c9ccd5c8328c2
kbzz <463661663@qq.com>
2018-05-11 15:54:59 +0800

    1

--- resources/views/Simple/Wechat/selftake_list.blade.php
+++ resources/views/Simple/Wechat/selftake_list.blade.php
@@ -3,48 +3,47 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>添加取货信息</title>
+	<title>我的取货地址</title>
 	<meta name="viewport" content="initial-scale=1, maximum-scale=1">
 	<meta name="apple-mobile-web-app-capable" content="yes">
 	<meta name="apple-mobile-web-app-status-bar-style" content="black">
 	<link rel="stylesheet" href="{{asset('public/Wechat')}}/css/public/light7.min.css">
-	<link rel="stylesheet" href="{{asset('public/Wechat')}}/css/addpugoods.css">
+	<link rel="stylesheet" href="{{asset('public/Wechat')}}/css/mypuinfo.css">
 </head>
 <body>
 <div class="page">
 	<div class="g-flexview">
-		<div class="main_block">
-			<div class="main_item">
-				<ul>
-					<li class="a_b_b a_b_t">
-						<div class="item_title title_f">姓名:</div>
-						<div class="item_input">
-							<input type="text" name="" placeholder="请填写取货人的姓名">
-						</div>
-						<div class="weixin_ress"></div>
-					</li>
-					<li class="a_b_b a_b_t">
-						<div class="item_title title_f"></div>
-						<div class="item_input item_input_pd1">
-							<input type="radio" id="userinfo2" name="dizhi" checked="checked" class="radio_address">
-							<label for="userinfo2">先生</label>
-							<input type="radio" id="userinfo1" name="dizhi" class="radio_address">
-							<label for="userinfo1">女士</label>
-						</div>
-					</li>
-					<li class="a_b_b a_b_t">
-						<div class="item_title title_f item_input_pd">电话:</div>
-						<div class="item_input item_input_pd">
-							<input type="text" name="" placeholder="请填写取货人的手机号码">
-						</div>
-					</li>
-				</ul>
+
+		<div class="max_height_box">
+			<div class="box">
+				<div class="alert_list">
+					<div class="radio_css">
+						<input type="radio" id="userinfo" name="dizhi" checked="checked" class="radio_address"><label for="userinfo">
+							薛志&nbsp;&nbsp;13824322924</label>
+					</div>
+					<div class="puinfo_btn">
+						<a href="javascript:;" class="puinfo_btn_xiugai">修改</a>
+						<a href="javascript:;" class="puinfo_btn_shanchu">删除</a>
+					</div>
+				</div>
+			</div>
+			<div class="box">
+				<div class="alert_list">
+					<div class="radio_css">
+						<input type="radio" id="userinfo1" name="dizhi" class="radio_address"><label for="userinfo1">
+							薛志&nbsp;&nbsp;13824322924</label>
+					</div>
+					<div class="puinfo_btn">
+						<a href="javascript:;" class="puinfo_btn_xiugai">修改</a>
+						<a href="javascript:;" class="puinfo_btn_shanchu">删除</a>
+					</div>
+				</div>
 			</div>
 		</div>
-		<a href="javascript:;" class="preservation">保存</a>
+		<a href="javascript:;" class="preservation">新增取货地址</a>
 	</div>
 	<!-- alert -->
-	<div class="popup_alert">
+	<div class="popup_alert alert" id="alert">
 		<div class="quhuo alert_width popup_alert_hook">
 			<p class="quhuoinfo">订单备注</p>
 			<div class="max_height_box">
@@ -52,7 +51,7 @@
 			</div>
 			<div class="alert_btn_wz">
 				<!-- <a href="javascript:;" class="btn_alert my_text_align btn_alert_bg1">取消</a> -->
-				<a href="javascript:;" class="btn_alert my_text_align btn_alert_bg" onclick="hide()">确认</a>
+				<a href="javascript:;" class="btn_alert my_text_align btn_alert_bg" onclick="hide('alert')">确认</a>
 			</div>
 		</div>
 	</div>
@@ -60,7 +59,6 @@
 </div>
 <script type='text/javascript' src='{{asset('public/Wechat')}}/js/public/jquery.min.js' charset='utf-8'></script>
 <script type='text/javascript' src='{{asset('public/Wechat')}}/js/public/light7.min.js' charset='utf-8'></script>
-<script type='text/javascript' src='{{asset('public/Wechat')}}/js/public/light7-city-picker.js' charset='utf-8'></script>
-<script type='text/javascript' src='{{asset('public/Wechat')}}/js/addpugoods.js' charset='utf-8'></script>
+<script type='text/javascript' src='{{asset('public/Wechat')}}/js/mypuinfo.js' charset='utf-8'></script>
 </body>
 </html>
