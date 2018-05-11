8f4f9fb15847b31adba44b73880b3813b3ffcb8b
xho <781966658.com>
2018-05-11 16:30:15 +0800

    x

--- app/Http/Controllers/Pay/WxController.php
+++ app/Http/Controllers/Pay/WxController.php
@@ -149,6 +149,8 @@ class WxController extends Controller
         $data["bill_type"] = "MCHT";
         // 填充数组
         $data = $this->fillData($param, "red_query");
+        var_dump($data);
+        exit;
         // 接口地址
         $url = "https://api.mch.weixin.qq.com/mmpaymkttransfers/gethbinfo";
         // 返回结果
