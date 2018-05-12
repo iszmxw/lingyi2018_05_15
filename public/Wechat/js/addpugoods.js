b11e9dda5cd5705cde4091d80a6037f58bd7f722
suolong520 <1172468462@qq.com>
2018-05-12 15:00:56 +0800

    0

--- public/Wechat/js/addpugoods.js
+++ public/Wechat/js/addpugoods.js
@@ -1,6 +1,3 @@
-$("#city-picker").cityPicker({
-    toolbarTemplate: '<header class="bar bar-nav">\
-    <button class="button button-link pull-right close-picker">确认</button>\
-    <h1 class="title">选择收货地址</h1>\
-    </header>'
-  });
+function select_morendizhi(){
+    alert();
+}
