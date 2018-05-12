5e3c2dd7f9fd11464d780b9e8f7e3c95cb8025a0
suolong520 <1172468462@qq.com>
2018-05-12 11:08:42 +0800

    0

--- resources/views/Simple/Wechat/online_order.blade.php
+++ resources/views/Simple/Wechat/online_order.blade.php
@@ -36,6 +36,14 @@
 					      </div>
 					    </div>
 			    	</div>
+                    <div class="item selftake_info_box" id="selftake_info_box" onclick="selftake_list()">
+				    	<div class="row">
+					      <div class="col-20 item_info"><span id="address_info_name">收货地址</span></div>
+					      <div class="col-80 item_info1 item_icon">
+							<p id="address_info"></p>
+					      </div>
+					    </div>
+			    	</div>
 				    <div class="item">
 					    <div class="row">
 					      <div class="col-20 item_info"><span>配送方式</span></div>
