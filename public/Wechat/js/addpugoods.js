da78dcc62286642855fb2d758d2e14fd67126573
suolong520 <1172468462@qq.com>
2018-05-12 16:08:19 +0800

    0

--- public/Wechat/js/addpugoods.js
+++ public/Wechat/js/addpugoods.js
@@ -14,7 +14,7 @@ function selftake_add_cm(){
     var data = $selftake_add.serialize();
     $.post(url,data,function(json){
         if(json.status==1){
-            console.log(json);
+            $.toast(json.data);
         }else if(json.status==0){
             $.toast(json.data);
         }
