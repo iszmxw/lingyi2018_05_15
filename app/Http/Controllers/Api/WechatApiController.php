9fa7591b496b407cb50839c2664e760c6fee14de
kbzz <463661663@qq.com>
2018-05-14 21:21:12 +0800

    1

--- app/Http/Controllers/Api/WechatApiController.php
+++ app/Http/Controllers/Api/WechatApiController.php
@@ -605,7 +605,7 @@ class WechatApiController extends Controller
                         $freight = $dispatch_info['freight'];
                     } else {
                         // 续重
-                        $additional_weight = $dispatch_info['first_weight'] - $weight;
+                        $additional_weight =  $weight - $dispatch_info['first_weight'];
                         // 续重费用
                         $freight = $dispatch_info['freight'] + ceil($additional_weight* $dispatch_info['renewal']);
                     }
