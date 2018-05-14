ae3407943d41d1763619039e6141cbb3e70b894e
suolong520 <1172468462@qq.com>
2018-05-14 10:52:55 +0800

    0

--- public/Wechat/js/newReceivingAddress.js
+++ public/Wechat/js/newReceivingAddress.js
@@ -1,23 +1,23 @@
-$("#city-picker").cityPicker({
-    toolbarTemplate: '<header class="bar bar-nav">\
-    <button class="button button-link pull-right close-picker">确认</button>\
-    <h1 class="title">选择收货地址</h1>\
-    </header>'
-  });
-  $(function(){
-      console.log($.smConfig.rawCitiesData);
-    $.smConfig.rawCitiesData = [
-        {
-            "name":"北dfgfd京",
-            "sub":[
-                {
-                    "name":"请选择"
-                },
-                {
-                    "name":"东城dfgdf区"
-                }
-            ],
-            "type":0
-        }
-    ]
-  });
+$(function(){
+    console.log($.smConfig.rawCitiesData);
+  $.smConfig.rawCitiesData = [
+      {
+          "name":"北dfgfd京",
+          "sub":[
+              {
+                  "name":"请选择"
+              },
+              {
+                  "name":"东城dfgdf区"
+              }
+          ],
+          "type":0
+      }
+  ]
+  $("#city-picker").cityPicker({
+      toolbarTemplate: '<header class="bar bar-nav">\
+      <button class="button button-link pull-right close-picker">确认</button>\
+      <h1 class="title">选择收货地址</h1>\
+      </header>'
+    });
+});
