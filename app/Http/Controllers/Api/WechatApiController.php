78c7cc53f936756b30fce5ff94fa31f8bf042e65
kbzz <463661663@qq.com>
2018-05-14 14:15:23 +0800

    1

--- app/Http/Controllers/Api/WechatApiController.php
+++ app/Http/Controllers/Api/WechatApiController.php
@@ -1352,7 +1352,7 @@ class WechatApiController extends Controller
      */
     public function select_address(Request $request)
     {
-      $list = Province::provinceList([]);
+      $list = Province::provinceList();
         print_r($list);exit;
 
 //        DB::beginTransaction();
