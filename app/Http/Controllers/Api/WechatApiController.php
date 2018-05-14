07f2ef3abeccdd4d0b4c7757408aea1b5315df51
kbzz <463661663@qq.com>
2018-05-14 15:44:10 +0800

    1

--- app/Http/Controllers/Api/WechatApiController.php
+++ app/Http/Controllers/Api/WechatApiController.php
@@ -1392,7 +1392,6 @@ class WechatApiController extends Controller
         $type = '0';
         foreach ($city as $key => $value) {
             $re = $this->area($value['id']);
-            dd($re);
             if ($re) {
                 $data[$key] = [
                     'name' => $value['city_name'],
