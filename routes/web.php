45b72036c32f8a96f6128b5c2d4af5b2a7293457
mail@54zm.com <mail@54zm.com>
2018-05-14 16:54:34 +0800

    'jy'

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,6 @@ use Illuminate\Support\Facades\Redis;
 */
 Route::any('/', function () {
     echo "你好世界";
-
 });
 /***************************框架学习整理资料部分***************************/
 Route::get('zeott', 'Tooling\TestController@test');
