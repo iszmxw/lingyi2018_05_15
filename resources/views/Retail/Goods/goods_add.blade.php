2343db2842ec76467b9cf0e4dd558cca79a2a6ee
kbzz <463661663@qq.com>
2018-05-15 11:32:52 +0800

    1

--- resources/views/Retail/Goods/goods_add.blade.php
+++ resources/views/Retail/Goods/goods_add.blade.php
@@ -102,16 +102,12 @@
                                                 <div class="col-sm-8">
                                                     <input type="number" class="form-control" name="weight">
                                                 </div>
-                                            </div>
-
-                                            <div class="line line-dashed b-b line-lg pull-in"></div>
-                                            <div class="form-group">
                                                 <label class="col-sm-2 control-label" for="input-id-1">指定运费</label>
                                                 <div class="col-sm-8">
                                                     <input type="number" class="form-control" name="freight_price">
                                                 </div>
                                             </div>
-
+                                            
                                             <div class="line line-dashed b-b line-lg pull-in"></div>
                                             <div class="form-group">
                                                 <label class="col-sm-2 control-label" for="input-id-1">商品详情</label>
