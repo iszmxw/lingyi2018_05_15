d9376f00749fec3226adc5e2da0f75ec3fd0252b
xho <781966658.com>
2018-05-15 17:24:12 +0800

    x

--- app/Http/Middleware/Pay/WxPayCheckAjax.php
+++ app/Http/Middleware/Pay/WxPayCheckAjax.php
@@ -476,6 +476,8 @@ class WxPayCheckAjax
         if (!$validate->passes()) {
             $error_msg = $validate->errors();
             $error_arr = json_decode(json_encode($error_msg, JSON_UNESCAPED_UNICODE), true);
+            var_dump($error_msg);
+            exit;
             foreach ($error_arr as $val) {
                 $error_msg = $val[0];
             }
