dffa33185de5a09551979d015ac4b91a7148b300
suolong520 <1172468462@qq.com>
2018-05-15 14:51:02 +0800

    0

--- public/Wechat/js/onlineOrder15-5.js
+++ public/Wechat/js/onlineOrder15-5.js
@@ -159,9 +159,6 @@ function selectRessId(address_id){
                         } else if (json.status == 0) {
                             $("#address_info_box").show();//显示收货地址列表
                             $("#dispatch_hook").hide(); 
-                             var order_price = $("#order_num_price").data("price");
-                             $("#order_btn_price").html("&yen;"+order_price);
-                             $("#order_num_price").html("&yen;"+order_price);
                             alert(json.msg);
                             return;
                         }
